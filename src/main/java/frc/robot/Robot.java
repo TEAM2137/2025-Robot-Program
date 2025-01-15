939df28e3227cd4a750742940c329437592b1dae
AveryG-883 <averyg883@icloud.com>
2025-01-15 18:58:32 -0500

    Moved BuildConstants to "generated" package

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.generated.BuildConstants;
 import frc.robot.generated.TunerConstants;
 import frc.robot.util.GameEvents;
 
