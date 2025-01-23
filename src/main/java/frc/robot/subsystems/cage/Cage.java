85206657ccdac5a08381271b4098a0cd76512342
Lightning-64 <averyg883@icloud.com>
2025-01-23 18:53:33 -0500

    Created Cage and CageIO classes

---
+++ src/main/java/frc/robot/subsystems/cage/Cage.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems.cage;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Cage extends SubsystemBase{
+    private final CageIO io;
+    private final CageIOInputsAutoLogged inputs;
+
+    public Cage(CageIO io){
+        this.io = io;
+        this.inputs = new CageIOInputsAutoLogged();
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Cage", inputs);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/cage/CageIO.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.cage;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface CageIO {
+    @AutoLog
+    public static class CageIOInputs {
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+        public double position = 0.0;
+    }
+
+    public default void updateInputs(CageIOInputs inputs) {}
+
+    public default void setPosition(double position) {}
+}
