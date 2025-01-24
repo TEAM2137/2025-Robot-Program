70a4a95b269074d73f5f69cce1aaf677146e1ad1
ThelastOfTheConclave <rjm.roland@gmail.com>
2025-01-23 19:10:43 -0500

    CageIOTalonFX class added

---
+++ src/main/java/frc/robot/subsystems/cage/CageIOTalonFX.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems.cage;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import frc.robot.subsystems.cage.CageIO.CageIOInputs;
+
+
+public class CageIOTalonFX implements CageIO {
+    private TalonFX motor = new TalonFX(0);
+
+    public CageIOTalonFX() {
+        var config = new TalonFXConfiguration();
+        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        motor.getConfigurator().apply(config);
+        motor.optimizeBusUtilization();
+    }
+
+    @Override
+    public void updateInputs(CageIOInputs inputs) {
+        inputs.appliedVolts = motor.getMotorVoltage().getValueAsDouble();
+        inputs.currentAmps = motor.getSupplyCurrent().getValueAsDouble();
+        // TODO the following rotational values need to be in meters and m/s
+        inputs.position = motor.getPosition().getValueAsDouble();
+    }
+
+    @Override
+    public void setPosition(double targetPosition) {
+        motor.setControl(new PositionVoltage(targetPosition));
+    }
+}
