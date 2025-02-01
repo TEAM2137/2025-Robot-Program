c09c81fd454ef60be56fc99fe96fad68b7b0a847
AveryG-883 <averyg883@icloud.com>
2025-02-01 15:22:02 -0500

    Fixed gearing (encoder is connected to shaft)

--- src/main/java/frc/robot/subsystems/cage/CageConstants.java
+++ src/main/java/frc/robot/subsystems/cage/CageConstants.java
@@ -5,6 +5,6 @@ public class CageConstants {
     public static final double kD = 0.0;
     public static final int motorID = 40;
     public static final double encoderZeroPosition = 0;
-    public static final double gearing = 4.0 * 4.0 * 4.0 * (42.0 / 18.0);
+    public static final double gearing = 42.0 / 18.0;
     public static final double motorPositionToArmAngle = 360.0;
 }
