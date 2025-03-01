890e26243af7eef2ba641af168797089c83356d1
AveryG-883 <averyg883@icloud.com>
2025-03-01 13:10:47 -0500

    Update at 'Sat Mar 01 13:10:47 EST 2025'

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -147,8 +147,8 @@ public class Autonomous {
         routine.active().onTrue(toReef1.resetOdometry().andThen(toReef1.cmd()));
 
         // Raise elevator on approach
-        // toReef1.atTimeBeforeEnd(elevatorDelay).onTrue(
-        //     robot.elevator.setPositionCommand(ElevatorConstants.L4));
+        toReef1.atTimeBeforeEnd(elevatorDelay).onTrue(
+            robot.elevator.setPositionCommand(ElevatorConstants.L4));
 
         // Score coral 1, stow elevator, and drive to pickup coral
         AutoCommands.createScoringSequence(scoreDuration, toReef1, toStation2, robot);
@@ -157,8 +157,8 @@ public class Autonomous {
         AutoCommands.createIntakeSequence(toStation2, toReef2, robot);
 
         // Raise elevator on approach
-        // toReef2.atTimeBeforeEnd(elevatorDelay).onTrue(
-        //     robot.elevator.setPositionCommand(ElevatorConstants.L4));
+        toReef2.atTimeBeforeEnd(elevatorDelay).onTrue(
+            robot.elevator.setPositionCommand(ElevatorConstants.L4));
 
         // Score coral 2, stow elevator, and drive to pickup coral
         AutoCommands.createScoringSequence(scoreDuration, toReef2, toStation3, robot);
@@ -167,8 +167,8 @@ public class Autonomous {
         AutoCommands.createIntakeSequence(toStation3, toReef3, robot);
 
         // Raise elevator on approach
-        // toReef3.atTimeBeforeEnd(elevatorDelay).onTrue(
-        //     robot.elevator.setPositionCommand(ElevatorConstants.L4));
+        toReef3.atTimeBeforeEnd(elevatorDelay).onTrue(
+            robot.elevator.setPositionCommand(ElevatorConstants.L4));
 
         // Score coral 3, stow elevator, and drive to pickup coral
         AutoCommands.createScoringSequence(scoreDuration, toReef3, toStation4, robot);
